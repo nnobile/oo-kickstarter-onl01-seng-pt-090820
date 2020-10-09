5b6193ea721de37327d7bff80d3f8fe91f96b071
Nick Nobile <nickcnobile@gmail.com>
2020-10-09 21:43:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,9 +8,9 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project, name)
+  def back_project(project)
     @backed_projects << project
-    project.backers << name
+    project.backers
   end
 
 
